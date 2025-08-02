8058069b295f1b8ef8d3a3a98c623e4705252ea4
Gary <62193371+gary111868@users.noreply.github.com>
2025-08-03 02:58:00 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nansenai/main.go
@@ -0,0 +1,23 @@
+// cmd/nansenai/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"nansenai/internal/nansenai"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := nansenai.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
